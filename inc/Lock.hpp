81c17acb4f567f83d68a245993c479c83ddda39d
Nicholas <nicholas@localhost.localdomain>
2014-09-11 21:07:09 +0800

    update

---
+++ inc/Lock.hpp
@@ -0,0 +1,26 @@
+#ifndef _LOCK_HPP_
+#define _LOCK_HPP_
+
+#include <sys/file.h>
+
+#include <cassert>
+
+class Lock {
+ private:
+  int fid;
+
+ public:
+  Lock() {
+    int fid = open("/tmp/.gtk-tiler.lock", O_CREAT | O_RDWR, 0666);
+    assert(fid != -1);
+
+    int lock = flock(fid, LOCK_EX | LOCK_NB);
+    assert(lock == 0);
+  };
+
+  ~Lock() {
+    close(fid);
+  }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+#include "inc/Lock.hpp"
 #include "inc/Process.hpp"
 #include "inc/Xlib.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+  Lock lock;
   Process::Daemonize(NULL);
 
   Xlib xlib;
